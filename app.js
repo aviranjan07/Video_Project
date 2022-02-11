ed31b0d81196eba49feea23a1e962158888dc31b
aviranjan07 <ad3447578@gmail.com>
2022-02-11 17:13:01 +0530

    add pause play functionality

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const video = document.querySelector(".video-container");
 btn.addEventListener("click", function() {
     if(!btn.classList.contains("slide")){
         btn.classList.add("slide");
+        video.pause();
     }else {
         btn.classList.remove("slide");
+        video.play();
     }
 });
